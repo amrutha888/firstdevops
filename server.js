af3a35530a0c6e1a5a1eed7e6cf119d7cb2da73b
Amrutha <amruthaachar08@gmail.com>
2026-06-07 22:03:40 +0530

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! from the manual CICD trigger, without K8s commands. This is my DevOps project 🚀');
+  res.send('Hello World! from the manual CICD trigger, without K8s commands. ');
 });
 
 app.listen(port, () => {
